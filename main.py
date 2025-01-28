617821cef37bc0a22bf7ed979068d2082e17d951
iampedrodominguez <pedro.dominguez@utec.edu.pe>
2025-01-27 20:40:07 -0500

    feat: added enpoint for subscribed apps

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ import requests
 
 load_dotenv(override=True)
 
-access_token = os.getenv("ACCESS_TOKEN")
-phone_number_id = os.getenv("PHONE_NUMBER_ID")
+WHATSAPP_BUSINESS_ID = os.getenv("WHATSAPP_BUSINESS_ID")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 
-print(access_token, phone_number_id)
+# print(access_token, phone_number_id)
 
-# # url = f"https://graph.facebook.com/{phone_number_id}"
+# url = f"https://graph.facebook.com/{phone_number_id}"
 # url = f"https://graph.facebook.com/{phone_number_id}/register"
-# headers = {
-#     "Authorization": f"Bearer {access_token}",
-# }
+headers = {
+    "Authorization": f"Bearer {ACCESS_TOKEN}",
+}
 # json = {
 #     "messaging_product": "whatsapp",
 #     "pin": "123456"
 # }
 # response = requests.post(url, headers=headers, json=json)
 # # response = requests.get(url, headers=headers)
-# print(response.json())
+
+
+
+url = f"https://graph.facebook.com/v21.0/{WHATSAPP_BUSINESS_ID}/subscribed_apps"
+response = requests.get(url, headers=headers)
+print(response.json())
