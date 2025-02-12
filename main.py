69251b3047c0e2b73cb37a2a8f2026a62732aeeb
iampedrodominguez <pedro.dominguez@utec.edu.pe>
2025-02-12 11:25:54 -0500

    feat: added new create template function

--- main.py
+++ main.py
@@ -9,22 +9,54 @@ WHATSAPP_BUSINESS_ID = os.getenv("WHATSAPP_BUSINESS_ID")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 
-# print(access_token, phone_number_id)
+def subscribed_apps():
+    url = f"https://graph.facebook.com/v21.0/{WHATSAPP_BUSINESS_ID}/subscribed_apps"
+    pass
 
-# url = f"https://graph.facebook.com/{phone_number_id}"
-# url = f"https://graph.facebook.com/{phone_number_id}/register"
-headers = {
+def info_phone_number():
+    url = f"https://graph.facebook.com/{PHONE_NUMBER_ID}"
+    headers = {
     "Authorization": f"Bearer {ACCESS_TOKEN}",
-}
-# json = {
-#     "messaging_product": "whatsapp",
-#     "pin": "123456"
-# }
-# response = requests.post(url, headers=headers, json=json)
-# # response = requests.get(url, headers=headers)
+    }
+    response = requests.get(url, headers=headers)
+    print(response.json())
 
+def register_phone_number():
+    url = f"https://graph.facebook.com/{PHONE_NUMBER_ID}/register"
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+    }
+    json = {
+        "messaging_product": "whatsapp",
+        "pin": "123456"
+    }
+    response = requests.post(url, headers=headers, json=json)
+    print(response.json())
 
+def create_product_update_template():
+    url = f"https://graph.facebook.com/{WHATSAPP_BUSINESS_ID}/message_templates"
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+    }
+    json = {
+    "name": "actualizacion_producto_v2",
+    "language": "es",
+    "category": "MARKETING",
+    "parameter_format": "POSITIONAL",
+    "components": [
+        {
+        "type": "BODY",
+        "text": "¡Hola {{1}}! 😊 Espero que tenga una linda {{2}} 🫶\n\n{{3}}\n\n¡Yo seguiré aquí, atendiendo a los pacientes! 💪",
+        "example": {
+            "body_text": [
+            [
+                "Dr. Alexander","mañana","Para agendar, ahora solo necesito el motivo de la consulta, ya no hace falta la duración. Si me equivoco en un horario o en algo, avíseme porfa."
+            ]
+            ]
+        }
+        }
+    ]
+    }
+    response = requests.post(url, headers=headers, json=json)
+    print(response.json())
 
-url = f"https://graph.facebook.com/v21.0/{WHATSAPP_BUSINESS_ID}/subscribed_apps"
-response = requests.get(url, headers=headers)
-print(response.json())
---
+++ push.bat
@@ -0,0 +1,5 @@
+git add .
+
+git commit -m "feat: added new create template function"
+
+git push
